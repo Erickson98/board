15f17f6ee32d108d528c51e2b2b02e073dbd1c08
Erickson <eagramonte@intellisys.com.do>
2024-05-12 10:25:54 -0400

    added drag and drop

---
+++ src/app/drag-drop/drag-drop.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DragDrop } from './drag-drop.component';
+
+describe('DragDrop', () => {
+  let component: DragDrop;
+  let fixture: ComponentFixture<DragDrop>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DragDrop]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(DragDrop);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/drag-drop/drag-drop.component.ts
@@ -0,0 +1,37 @@
+import {Component} from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+
+/**
+ * @title Drag&Drop connected sorting
+ */
+@Component({
+  selector: 'drag-drop',
+  templateUrl: 'drag-drop.component.html',
+  styleUrl: 'drag-drop.component.css',
+  standalone: true,
+  imports: [CdkDropList, CdkDrag],
+})
+export class DragDrop {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+}
