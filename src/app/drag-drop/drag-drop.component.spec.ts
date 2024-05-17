6a3b4e6abbb60d1819c6c0f47918224d37a8ad49
Erickson <eagramonte@intellisys.com.do>
2024-05-17 19:30:20 -0400

    add the first board, add more cards

--- src/app/drag-drop/drag-drop.component.spec.ts
+++ src/app/drag-drop/drag-drop.component.spec.ts
@@ -9,7 +9,7 @@ describe('DragDrop', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DragDrop]
-    })
+    }) 
     .compileComponents();
     
     fixture = TestBed.createComponent(DragDrop);
--- src/app/drag-drop/drag-drop.component.ts
+++ src/app/drag-drop/drag-drop.component.ts
@@ -6,6 +6,7 @@ import {
   CdkDrag,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
+import {MatIconModule} from '@angular/material/icon';
 
 /**
  * @title Drag&Drop connected sorting
@@ -15,12 +16,25 @@ import {
   templateUrl: 'drag-drop.component.html',
   styleUrl: 'drag-drop.component.css',
   standalone: true,
-  imports: [CdkDropList, CdkDrag],
+  imports: [CdkDropList, CdkDrag, MatIconModule],
 })
 export class DragDrop {
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  todo = [ 
+    'Colocar cualquier imagen pasada por una url',
+    'Boton para añadir mas tarjeta',
+    'Ver si las tarjetas se pueden clickear',
+    'Cambiar titulo a las tarjetas',
+    'Crear la ruta home que servira para los tableros',
+  ];
 
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  done:string[] = [];
+  doing:string[] = []
+
+  addCard() {
+    this.doing.push("Doing")
+    console.log('Botón presionado: Añadir una tarjeta');
+    // Aquí puedes añadir la lógica que necesitas
+  }
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
