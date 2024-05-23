875b735ee7d316ed6a11953437cc2a715e26c05d
Erickson <eagramonte@intellisys.com.do>
2024-05-23 19:30:08 -0400

    testing of indexdb

--- src/app/drag-drop/drag-drop.component.ts
+++ src/app/drag-drop/drag-drop.component.ts
@@ -32,8 +32,7 @@ import { CommonModule } from '@angular/common';
 })
 export class DragDrop implements AfterViewInit {
   todo = [ 
-    'Colocar cualquier imagen pasada por una url', 
-    
+    'Colocar cualquier imagen pasada por una url',  
     'Cambiar titulo a las tarjetas',
     'Crear la ruta home que servira para los tableros',
   ];
@@ -46,6 +45,65 @@ export class DragDrop implements AfterViewInit {
   constructor(private cdr: ChangeDetectorRef) {}
 
   @ViewChild('nameInput', { static: false }) nameInput !: ElementRef;
+
+  ngOnInit() {
+    this.initDatabase();
+  }
+
+  initDatabase() {
+    const request = indexedDB.open('MyDatabase', 1);
+
+    request.onupgradeneeded = function(event) {
+      const db = (event.target as IDBOpenDBRequest).result;
+
+      // Crear un almacén de objetos para "Columnas"
+      const columnasStore = db.createObjectStore('Columnas', { keyPath: 'name' });
+
+      // Crear almacenes de objetos para "Columna1" y "Columna2"
+      const columna1Store = db.createObjectStore('Columna1', { keyPath: 'id' });
+      const columna2Store = db.createObjectStore('Columna2', { keyPath: 'id' });
+
+      // Crear índices en "Columna1" y "Columna2" según sea necesario
+      columna1Store.createIndex('foreign_key_id', 'foreign_key_id', { unique: false });
+      columna2Store.createIndex('foreign_key_id', 'foreign_key_id', { unique: false });
+
+      // Crear almacén de objetos para "Comments"
+      const commentsStore = db.createObjectStore('Comments', { keyPath: 'id' });
+      commentsStore.createIndex('Author', 'Author', { unique: false });
+    };
+
+    request.onsuccess = (event) => {
+      const db = (event.target as IDBOpenDBRequest).result;
+      this.getData(db);
+    };
+
+    request.onerror = function(event) {
+      console.error('Database error:');
+    };
+  }
+
+  getData(db: IDBDatabase) {
+    const transaction = db.transaction(['Columnas', 'Columna1', 'Columna2', 'Comments'], 'readonly');
+
+    const columna1Store = transaction.objectStore('Columna1');
+    const columna1Request = columna1Store.getAll();
+
+    columna1Request.onsuccess = (event) => {
+      this.doing = (event.target as IDBRequest).result;
+      console.log('Columna1:', this.doing);
+    };
+
+    transaction.oncomplete = function() {
+      console.log('All data retrieved from the database.');
+    };
+
+    transaction.onerror = function(event) {
+      console.error('Transaction error:', (event.target as IDBTransaction).error);
+    };
+  }
+
+  
+
 
    ngAfterViewInit() {
     this.setFocus();
@@ -97,8 +155,16 @@ export class DragDrop implements AfterViewInit {
   }
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
+      console.log(event.container.data)
+      console.log(event.previousIndex)
+      console.log(event.currentIndex)
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
+      console.log(event.item.element.nativeElement.innerHTML)
+      console.log(event.previousContainer.data)
+      console.log(event.container.data)
+      console.log(event.previousIndex)
+      console.log(event.currentIndex)
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
