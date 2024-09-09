c3a90bfb1c252416fb016f892c01e5b78d4db58e
Erickson Ruck <eagramonte@intellisys.com.do>
2024-09-09 16:51:29 -0400

    add textarea

--- src/app/tab-card/tab-card.component.ts
+++ src/app/tab-card/tab-card.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { TabViewModule } from 'primeng/tabview';
 import { MatListModule } from '@angular/material/list';
+import { CheckboxModule } from 'primeng/checkbox';
 
 /**
  * @title Tab group with asynchronously loading tab contents
@@ -11,7 +12,7 @@ import { MatListModule } from '@angular/material/list';
   templateUrl: 'tab-card.component.html',
   styleUrl: 'tab-card.component.css',
   standalone: true,
-  imports: [MatTabsModule, TabViewModule, MatListModule],
+  imports: [MatTabsModule, TabViewModule, MatListModule, CheckboxModule],
 })
 export class TabCard {
   typesOfShoes: string[] = [
@@ -21,4 +22,12 @@ export class TabCard {
     'Moccasins',
     'Sneakers',
   ];
+  auto_grow(element: any) {
+    console.log('first');
+    // Restablecer la altura temporalmente para evitar errores de cálculo
+    console.log(element.style.height);
+    element.style.height = 'auto';
+    // Ajustar la altura al scrollHeight del contenido
+    element.style.height = element.scrollHeight + 'px';
+  }
 }
